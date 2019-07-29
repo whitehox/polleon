bfb5012e4cb56a7c671f39d202ec2532cede56ee
Ibrahim Joseph <josephibrahi@gmail.com>
2019-07-29 11:57:43 +0100

    added start and end date, votes-per-contestants to EventType

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -3,6 +3,9 @@ import { Document } from "mongoose";
 export interface EventType {
   title: string;
   description: string;
+  start_date: string;
+  end_date: string;
+  votes_per_contestant: number;
 }
 
 export interface EventModel extends EventType, Document {}
