1e08005e35ef70fb4a8a2325d61a56d0010a5f9b
Ibrahim Joseph <josephibrahi@gmail.com>
2019-07-29 13:32:45 +0100

    created rest api for adding events

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const router = express.Router();
+import { addEvent } from '../controllers';
 
 /* GET home page. */
 router.get('/', function(
@@ -8,6 +9,15 @@ router.get('/', function(
   _next: express.NextFunction
 ) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/event', (req: express.Request, res: express.Response) => {
+  const data = addEvent(req.body);
+  try {
+    res.status(200).json({ data });
+  } catch {
+    res.status(400).json({ error: 'An error occured' });
+  }
 });
 
 export default router;
