0f6aaa94bb5c077228402486c0adc90b413c8f6c
Ibrahim Joseph <josephibrahi@gmail.com>
2019-07-29 12:06:43 +0100

    Updated event and user types

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,11 +1,27 @@
-import { Document } from "mongoose";
+import { Document } from 'mongoose';
 
 export interface EventType {
+  user_id: string;
   title: string;
   description: string;
   start_date: string;
   end_date: string;
   votes_per_contestant: number;
+  event_type: string;
+  created_at: Date;
+}
+
+export interface UserType {
+  user_id: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  company_name?: string;
+  profile_image?: string;
+  email: string;
+  phone_number?: string;
+  password: string;
+  created_at: Date;
 }
 
 export interface EventModel extends EventType, Document {}
