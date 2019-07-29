d6c6c8bce1db1ccc8f7db5f798ab446b8a7d0392
Ibrahim Joseph <josephibrahi@gmail.com>
2019-07-29 11:57:59 +0100

    added start and end date, votes-per-contestants to EventType

--- src/models/events.ts
+++ src/models/events.ts
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 import { EventModel } from "../typings";
 
 const EventSchema = new mongoose.Schema({
+  user_id: String,
   title: String,
   description: String,
   start_date: Date,
-  end_date: Date
+  end_date: Date,
+  votes_per_contestant: Number
 });
 
 export default mongoose.model<EventModel>("Event", EventSchema);
