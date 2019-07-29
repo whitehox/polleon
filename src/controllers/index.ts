e6090066115cc58140fdefb0f48c356a2c97284e
Ibrahim Joseph <josephibrahi@gmail.com>
2019-07-29 15:23:06 +0100

    completed create and get user functionalities

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -10,3 +10,12 @@ export function addEvent(eventInput: EventInput) {
   const event = new Event(eventInput);
   return event.save();
 }
+
+export function addUser(userInput: UserType) {
+  const user = new User(userInput);
+  return user.save();
+}
+
+export function getUsers() {
+  return User.find();
+}
