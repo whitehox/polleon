b586c386bdc0f288927d655bb0e872b559738997
Ibrahim Joseph <josephibrahi@gmail.com>
2019-07-29 15:24:28 +0100

    Moved event and user types to separate modules

---
+++ src/types/users.ts
@@ -0,0 +1,100 @@
+/**
+ * GraphQL user schema types
+ */
+
+import { GraphQLObjectType } from 'graphql';
+import { GraphQLString } from 'graphql';
+import { GraphQLInputObjectType } from 'graphql';
+
+export const UserType = new GraphQLObjectType({
+  name: 'UserType',
+  description: 'Users type',
+  fields: () => ({
+    username: {
+      type: GraphQLString,
+      description: 'The username',
+      resolve: source => source.username
+    },
+    firstname: {
+      type: GraphQLString,
+      description: 'The users firstname',
+      resolve: source => source.firstname
+    },
+    lastname: {
+      type: GraphQLString,
+      description: 'The users lastname',
+      resolve: source => source.lastname
+    },
+    company_name: {
+      type: GraphQLString,
+      description: 'The users company name'
+    },
+    profile_image: {
+      type: GraphQLString,
+      description: 'The users company name'
+    },
+    email: {
+      type: GraphQLString,
+      description: 'The users email address'
+    },
+    phone_number: {
+      type: GraphQLString,
+      description: 'The users phone number'
+    },
+    password: {
+      type: GraphQLString,
+      description: 'The users password'
+    },
+    account_type: {
+      type: GraphQLString,
+      description: 'The user account type'
+    },
+    created_at: {
+      type: GraphQLString,
+      description: 'Date the account was created'
+    }
+  })
+});
+
+export const UserInput = new GraphQLInputObjectType({
+  name: 'UserInput',
+  description: 'Users type',
+  fields: () => ({
+    username: {
+      type: GraphQLString,
+      description: 'The username'
+    },
+    firstname: {
+      type: GraphQLString,
+      description: 'The users firstname'
+    },
+    lastname: {
+      type: GraphQLString,
+      description: 'The users lastname'
+    },
+    company_name: {
+      type: GraphQLString,
+      description: 'The users company name'
+    },
+    profile_image: {
+      type: GraphQLString,
+      description: 'The users company name'
+    },
+    email: {
+      type: GraphQLString,
+      description: 'The users email address'
+    },
+    phone_number: {
+      type: GraphQLString,
+      description: 'The users phone number'
+    },
+    password: {
+      type: GraphQLString,
+      description: 'The users password'
+    },
+    created_at: {
+      type: GraphQLString,
+      description: 'Date the account was created'
+    }
+  })
+});
