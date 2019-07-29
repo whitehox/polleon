3e7afe922e3d26ab3410c9cb67ee249c38195da9
Ibrahim Joseph <josephibrahi@gmail.com>
2019-07-29 13:33:16 +0100

    Completed addEvent function

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,12 @@
-import Event from "../models/events";
+import Event from '../models/events';
+
+import { EventInput } from '../typings';
 
 export function getEvents() {
   return Event.find();
+}
+
+export function addEvent(eventInput: EventInput) {
+  const event = new Event(eventInput);
+  return event.save();
 }
