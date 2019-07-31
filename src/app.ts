355bb71439938a713450779f87bd38860b9c8b45
whitehox <oketegah@gmail.com>
2019-07-31 10:58:36 -0700

    moved cors above middlewares

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import indexRouter from './routes/index';
 
 const app = express();
 
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
