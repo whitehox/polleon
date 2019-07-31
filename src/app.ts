2d318cf1a7e196bd65de367b14f92dacd2145551
whitehox <oketegah@gmail.com>
2019-07-31 10:59:39 -0700

    jsonwebtoken installed

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
 app.use('/api', indexRouter);
 app.use(
   '/graphql',
