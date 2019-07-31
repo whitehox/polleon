474822d37650ca4767695cb5ee18a8e43d80e457
whitehox <oketegah@gmail.com>
2019-07-31 10:57:37 -0700

    removed apolloclient from root

--- client/src/index.js
+++ client/src/index.js
@@ -10,21 +10,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3005/graphql'
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
